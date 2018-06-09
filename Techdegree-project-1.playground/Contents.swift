d2118676bf3d275294a4581b780d681a26bc6000
Bartlomiej Kredek <bkredek@gmail.com>
2018-06-09 20:49:30 +0200

    Leauge edit

--- Techdegree-project-1.playground/Contents.swift
+++ Techdegree-project-1.playground/Contents.swift
@@ -2,38 +2,45 @@
 
 import UIKit
 
-//Players
+//Teams, experience players and The Leauge
 
-let player1: [String: Any] = ["name": "Joe Smith",
-                              "height": 42,
-                              "experience": true,
-                              "guardian name": "Jim and Jan Smith"]
+var teams: [[String: Any]] = []
 
-let player2: [String: Any] = ["name": "Jill Tanner",
-                              "height": 36,
-                              "experience": true,
-                              "guardian name": "Clara Tanner"]
+var experiencedPlayers: [[String: Any]] = []
 
-let player3: [String: Any] = ["name": "Bill Bon",
-                              "height": 43,
-                              "experience": true,
-                              "guardian name": "Sara and Jenny Bon"]
+var inexperiencedPlayers: [[String: Any]] = []
 
-let player4: [String: Any] = ["name": "Eva Gordon",
-                              "height": 45,
-                              "experience": false,
-                              "guardian name": "Wendy and Mike Gordon"]
+var TheLeauge: [[String: Any]] = [["name": "Joe Smith",
+                                   "height": 42,
+                                   "experience": true,
+                                   "guardian name": "Jim and Jan Smith"],
+                                  ["name": "Jill Tanner",
+                                   "height": 36,
+                                   "experience": true,
+                                   "guardian name": "Clara Tanner"],
+                                  ["name": "Bill Bon",
+                                   "height": 43,
+                                   "experience": true,
+                                   "guardian name": "Sara and Jenny Bon"],
+                                  ["name": "Eva Gordon",
+                                   "height": 45,
+                                   "experience": false,
+                                   "guardian name": "Wendy and Mike Gordon"]]
 
-//Teams, experience players
+//Putting experience and inexperience players in right collections
 
-
-
-
-
-
-
-
-
+for player in TheLeauge {
+    let experience = player["experience"] as! Bool
+    
+    if experience
+    {
+        experiencedPlayers.append(player)
+    }
+    else
+    {
+        inexperiencedPlayers.append(player)
+    }
+}
 
 
 
