ae689d81f8c8d9134d1aa87aafefcd26328428ef
Bartlomiej Kredek <bkredek@gmail.com>
2018-06-09 15:30:01 +0200

    Added playground
    Added some players

---
+++ Techdegree-project-1.playground/Contents.swift
@@ -0,0 +1,69 @@
+//Tech Degree Project 1
+
+import UIKit
+
+//Players
+
+let player1: [String: Any] = ["name": "Joe Smith",
+                              "height": 42,
+                              "experience": true,
+                              "guardian name": "Jim and Jan Smith"]
+
+let player2: [String: Any] = ["name": "Jill Tanner",
+                              "height": 36,
+                              "experience": true,
+                              "guardian name": "Clara Tanner"]
+
+let player3: [String: Any] = ["name": "Bill Bon",
+                              "height": 43,
+                              "experience": true,
+                              "guardian name": "Sara and Jenny Bon"]
+
+let player4: [String: Any] = ["name": "Eva Gordon",
+                              "height": 45,
+                              "experience": false,
+                              "guardian name": "Wendy and Mike Gordon"]
+
+//Teams, experience players
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
